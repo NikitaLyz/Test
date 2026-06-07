e3f576af240cbc2ec9dff3fa747bbbabae437951
NikitaLyz <nikitaxd2018@gmail.com>
2026-06-07 15:41:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Конфігурація підключення до платіжного шлюзу Stripe та AWS
-
+GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz1234567890ABCDEF"
 AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 STRIPE_API_KEY = "sk_live_51NxabcXYZ123fakeKeyDoNotUseOnProd!!!"
 DB_PASSWORD = "MySuperSecurePassword2026!"
